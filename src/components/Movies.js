84e3dadbc5482c56ad4adb4a25b267fa862e9030
nick <mwauranicholas10@gmail.com>
2023-01-24 10:57:20 +0300

    completed components

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,16 @@
 import React from "react";
+//import { Link } from "react-router-dom";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>
+      Movies Page
+    </h1>
+    {movies.map((item)=>{
+      return <div key={item.title} > {item.title} {item.time} <ul>{item.genres.map(item=><li key={item}>{item}</li>)}</ul></div>
+    })}
+  </div>;
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+    <ul>
+      <li> <NavLink to="/"  activeStyle={{color:"green"}}>
+        Home
+      </NavLink></li>
+      <li> <NavLink to="/movies"  activeStyle={{color:"green"}}>
+        Movies
+      </NavLink></li>
+      <li> <NavLink to="/directors"  activeStyle={{color:"green"}}>
+        Directors
+      </NavLink></li>
+      <li> <NavLink to="/actors"  activeStyle={{color:"green"}}>
+        Actors
+      </NavLink></li>
+    </ul>
+    </div>
+     
+  )
 }
 
 export default NavBar;
